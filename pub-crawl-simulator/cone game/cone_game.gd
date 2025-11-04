cf30103922ca25617291272d66a7d397ad7dcac4
JamesSknr <8jamesskinner8@gmail.com>
2025-11-04 13:34:24 +0000

    Fix-Game-Win-Logic

--- pub-crawl-simulator/cone game/cone_game.gd
+++ pub-crawl-simulator/cone game/cone_game.gd
@@ -12,15 +12,18 @@ var area_inside: bool = false
 func _process(delta: float) -> void:
 	if area_inside:
 		entered_time += delta
-		if entered_time >= 2.0 and cone.linear_velocity == Vector2.ZERO: # 1 second threshold
+		# Check that the cone has landed in the correctd zone and is statinoary
+		if entered_time >= 2.0 and cone.linear_velocity == Vector2.ZERO:
 			print("CONE LANDED")
 			victory_label.visible = true
 			
 
 func _on_area_2d_area_entered(area: Area2D) -> void:
+	print("ENTERED")
 	entered_time = 0.0
 	area_inside = true
 
 func _on_area_2d_area_exited(area: Area2D) -> void:
+	print("EXITED")
 	area_inside = false
 	entered_time = 0.0
